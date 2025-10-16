4547b24e18d4a38d817c138092f788e41dbec02f
bialger <bigulov.sasha@gmail.com>
2025-10-16 22:32:09 +0300

    refactor: add TokenPosition class for managing line and column tracking in lexer

--- lib/lexer/tokens/TokenPosition.hpp
+++ lib/lexer/tokens/TokenPosition.hpp
@@ -5,16 +5,11 @@
 
 class TokenPosition {
 public:
-  TokenPosition(int32_t line, int32_t column) : line_(line), column_(column) {
-  }
+  TokenPosition(int32_t line, int32_t column);
 
-  [[nodiscard]] int32_t GetLine() const noexcept {
-    return line_;
-  }
+  [[nodiscard]] int32_t GetLine() const noexcept;
 
-  [[nodiscard]] int32_t GetColumn() const noexcept {
-    return column_;
-  }
+  [[nodiscard]] int32_t GetColumn() const noexcept;
 
 private:
   int32_t line_;
