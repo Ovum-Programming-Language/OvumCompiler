e5af7022c3c58467d7c2b6894145b99de4e69e62
biqiboqi <biqiboqi@gmail.com>
2025-10-18 01:51:14 +0300

    feat: add nan and inf float handling, add in keyword

--- bin/main.cpp
+++ bin/main.cpp
@@ -3,14 +3,7 @@
 #include "lib/compiler_ui/compiler_ui_functions.hpp"
 
 int main(int32_t argc, char** argv) {
-  const std::string sample = R"ovum(
-// demo
-fun Main(args: StringArray): Int {
-    val count: Int = args.Length()
-    sys::Print("Args count: " + count.ToString())
-    return 0
-}
-)ovum";
+  const std::string sample = R"ovum(NaN)ovum";
 
   return StartCompilerConsoleUI({"ovumc", sample}, std::cout, std::cerr);
 }
--- lib/lexer/handlers/IdentifierHandler.cpp
+++ lib/lexer/handlers/IdentifierHandler.cpp
@@ -1,11 +1,30 @@
 #include "IdentifierHandler.hpp"
 
+#include <limits>
+#include <cmath>
+
 #include "lib/lexer/tokens/TokenFactory.hpp"
 
 OptToken IdentifierHandler::Scan(SourceCodeWrapper& wrapper) {
   std::string s;
   s.push_back(wrapper.CurrentChar());
-  wrapper.ConsumeWhile(s, [](char ch) { return std::isalnum(static_cast<unsigned char>(ch)) || ch == '_'; });
+  wrapper.ConsumeWhile(s,
+                       [](char ch) {
+                         return std::isalnum(static_cast<unsigned char>(ch)) || ch == '_';
+                       });
+
+  if (s == "Inf" || s == "inf" || s == "Infinity" || s == "infinity") {
+    return std::make_optional(
+        TokenFactory::MakeFloatLiteral(s,
+                                       std::numeric_limits<long double>::infinity(),
+                                       wrapper.GetLine(),
+                                       wrapper.GetTokenCol()));
+  }
+
+  if (s == "NaN") {
+    return std::make_optional(
+        TokenFactory::MakeFloatLiteral(s, std::nanl(""), wrapper.GetLine(), wrapper.GetTokenCol()));
+  }
 
   if (wrapper.IsKeyword(s)) {
     if (s == "true" || s == "false") {
