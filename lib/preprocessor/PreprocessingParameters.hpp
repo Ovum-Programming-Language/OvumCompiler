79c0d77c3b593e673191b769c5ec272739c488c5
bialger <bigulov.sasha@gmail.com>
2025-10-22 15:31:32 +0300

    refactor: change include_paths type to std::filesystem::path for better path handling

--- lib/preprocessor/PreprocessingParameters.hpp
+++ lib/preprocessor/PreprocessingParameters.hpp
@@ -1,12 +1,13 @@
 #ifndef PREPROCESSINGPARAMETERS_HPP_
 #define PREPROCESSINGPARAMETERS_HPP_
 
+#include <filesystem>
 #include <set>
 #include <string>
 #include <unordered_set>
 
 struct PreprocessingParameters {
-  std::set<std::string> include_paths;
+  std::set<std::filesystem::path> include_paths;
   std::unordered_set<std::string> predefined_symbols;
 };
 
