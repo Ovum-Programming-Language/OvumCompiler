1094bf4300d2b8b6e50df1d293d1c87ae3861787
biqiboqi <biqiboqi@gmail.com>
2025-11-03 01:04:30 +0300

    fix: fixed SafeCall

--- lib/parser/ast/nodes/exprs/SafeCall.hpp
+++ lib/parser/ast/nodes/exprs/SafeCall.hpp
@@ -2,16 +2,21 @@
 #define SAFECALL_HPP_
 
 #include <memory>
+#include <optional>
+#include <string>
 #include <vector>
 
 #include "lib/parser/ast/nodes/base/Expr.hpp"
+#include "lib/parser/types/TypeReference.hpp"
+
+class AstVisitor;
 
 class SafeCall : public Expr {
 public:
   void Accept(AstVisitor& v) override;
 
-  [[nodiscard]] const Expr* Object() const noexcept;
-  [[nodiscard]] Expr* MutableObject() noexcept;
+  [[nodiscard]] const Expr& Object() const noexcept;
+  [[nodiscard]] Expr& MutableObject() noexcept;
   void SetObject(std::unique_ptr<Expr> obj);
   std::unique_ptr<Expr> ReplaceObject(std::unique_ptr<Expr> obj);
 
@@ -21,21 +26,19 @@ public:
   [[nodiscard]] const std::vector<std::unique_ptr<Expr>>& Args() const noexcept;
   [[nodiscard]] std::vector<std::unique_ptr<Expr>>& MutableArgs() noexcept;
   void AddArg(std::unique_ptr<Expr> e);
-
   void ClearArgs();
 
   [[nodiscard]] bool IsNullPropagating() const noexcept;
 
   void SetInferredType(TypeReference t);
-  [[nodiscard]] const TypeReference* InferredType() const noexcept;
+  [[nodiscard]] const std::optional<TypeReference>& InferredType() const noexcept;
 
 private:
   std::unique_ptr<Expr> object_;
   std::string method_;
   std::vector<std::unique_ptr<Expr>> args_;
 
-  TypeReference inferred_type_;
-  bool inferred_type_set_ = false;
+  std::optional<TypeReference> inferred_type_;
 };
 
 #endif // SAFECALL_HPP_
