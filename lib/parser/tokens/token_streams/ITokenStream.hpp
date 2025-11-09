f675de4570e3b5cd0bc3e2b7e93b0f51db6a6168
biqiboqi <biqiboqi@gmail.com>
2025-11-09 23:03:43 +0300

    feat: add vector token stream

--- lib/parser/tokens/token_streams/ITokenStream.hpp
+++ lib/parser/tokens/token_streams/ITokenStream.hpp
@@ -9,19 +9,15 @@ class ITokenStream {
 public:
   virtual ~ITokenStream() = default;
 
-  virtual const Token& Peek(std::size_t k = 0) = 0;
-
+  virtual const Token& Peek(size_t k = 0) = 0;
   virtual TokenPtr Consume() = 0;
 
   virtual size_t Position() const = 0;
-
-  virtual void Rewind(std::size_t n) = 0;
-
+  virtual void Rewind(size_t n) = 0;
   virtual bool IsEof() const = 0;
 
   virtual const Token* LastConsumed() const = 0;
-
-  virtual const Token* TryPeek(std::size_t k = 0) = 0;
+  virtual const Token* TryPeek(size_t k = 0) = 0;
 };
 
 #endif // ITOKENSTREAM_HPP_
