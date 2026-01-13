5416a586b11d1bd782064c6458ab53ab45f086e2
bialger <bigulov.sasha@gmail.com>
2026-01-13 08:36:53 +0300

    feat: add detailed help message and improve error handling in ProjectIntegrationTestSuite tests

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -7,9 +7,17 @@
 #include "test_functions.hpp"
 #include "test_suites/ProjectIntegrationTestSuite.hpp"
 
-TEST_F(ProjectIntegrationTestSuite, InitTest) {
-  ASSERT_TRUE(std::filesystem::is_directory(kTemporaryDirectoryName));
-}
+static const std::string kHelpMessage =
+    "ovumc\n"
+    "Ovum Compiler that compiles Ovum source code to Ovum Intermediate Language.\n\n"
+    "OPTIONS:\n"
+    "-D,  --define-symbols=<string>:  Defined symbols [repeated]\n"
+    "-I,  --include-dirs=<CompositeString>:  Path to directories where include files are located [repeated]\n"
+    "-m,  --main-file=<CompositeString>:  Path to the main file\n"
+    "-o,  --output-file=<CompositeString>:  Path to the output file [default = Same as input file but with .oil "
+    "extension]\n"
+    "-n,  --no-lint:  Disable linter [repeated]\n\n"
+    "-h,  --help:  Display this help and exit\n";
 
 // Test help output
 TEST_F(ProjectIntegrationTestSuite, HelpOutput) {
@@ -21,15 +29,14 @@ TEST_F(ProjectIntegrationTestSuite, HelpOutput) {
   std::ostringstream err_h;
   ASSERT_EQ(StartCompilerConsoleUI(SplitString("ovumc -h"), out_h, err_h), 0);
   ASSERT_FALSE(out_h.str().empty());
-  ASSERT_TRUE(out_h.str().find("ovumc") != std::string::npos);
-  ASSERT_TRUE(out_h.str().find("main-file") != std::string::npos || out_h.str().find("main_file") != std::string::npos);
+  ASSERT_EQ(out_h.str(), kHelpMessage);
 
   // Test with --help flag
   std::ostringstream out_help;
   std::ostringstream err_help;
   ASSERT_EQ(StartCompilerConsoleUI(SplitString("ovumc --help"), out_help, err_help), 0);
   ASSERT_FALSE(out_help.str().empty());
-  ASSERT_TRUE(out_help.str().find("ovumc") != std::string::npos);
+  ASSERT_EQ(out_help.str(), kHelpMessage);
 }
 
 // Test wrong arguments
@@ -37,14 +44,14 @@ TEST_F(ProjectIntegrationTestSuite, WrongArgsTestNoArguments) {
   std::ostringstream out;
   std::ostringstream err;
   ASSERT_EQ(StartCompilerConsoleUI(SplitString("ovumc"), out, err), 1);
-  ASSERT_FALSE(err.str().empty());
+  ASSERT_EQ(err.str(), "Not enough values were passed to argument --main-file.\n" + kHelpMessage);
 }
 
 TEST_F(ProjectIntegrationTestSuite, WrongArgsTestInvalidFile) {
   std::ostringstream out;
   std::ostringstream err;
   ASSERT_EQ(StartCompilerConsoleUI(SplitString("ovumc -m nonexistent_file.ovum"), out, err), 1);
-  ASSERT_FALSE(err.str().empty());
+  ASSERT_EQ(err.str(), "An incorrect value was passed to the --main-file argument.\n" + kHelpMessage);
 }
 
 TEST_F(ProjectIntegrationTestSuite, WrongArgsTestInvalidIncludeDirectory) {
@@ -58,14 +65,14 @@ TEST_F(ProjectIntegrationTestSuite, WrongArgsTestInvalidIncludeDirectory) {
   std::ostringstream err;
   ASSERT_EQ(
       StartCompilerConsoleUI(SplitString("ovumc -m " + test_file.string() + " -I nonexistent_directory"), out, err), 1);
-  ASSERT_FALSE(err.str().empty());
+  ASSERT_EQ(err.str(), "An incorrect value was passed to the --include-dirs argument.\n" + kHelpMessage);
 }
 
 TEST_F(ProjectIntegrationTestSuite, WrongArgsTestMissingMainFile) {
   std::ostringstream out;
   std::ostringstream err;
   ASSERT_EQ(StartCompilerConsoleUI(SplitString("ovumc -o output.oil"), out, err), 1);
-  ASSERT_FALSE(err.str().empty());
+  ASSERT_EQ(err.str(), "Not enough values were passed to argument --main-file.\n" + kHelpMessage);
 }
 
 // Example files compilation tests
